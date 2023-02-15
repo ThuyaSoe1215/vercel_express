06a0b3a762c176f46b6cc7c59a8b85619c51568c
thuya soe <thuyasoe1215@gmai.com>
2023-02-15 21:34:52 +0630

    added image upload to digital ocean

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express"
+import aws from "aws-sdk"
 import dotenv from "dotenv"
+import bodyParser from "body-parser";
+import formidable from "formidable"
+import { v4 as uuidv4 } from 'uuid';
+import fs from "fs"
 dotenv.config();
 const app = express();
+app.use(bodyParser.json())
 const port = 5000;
 const apiUrl = process.env.API_URL;
+
+const s3 = new aws.S3({
+  endpoint: "sgp1.digitaloceanspaces.com",
+});
 
 const html = `
     <!DOCTYPE html>
@@ -31,6 +41,32 @@ app.get('/api', (req: Request, res: Response) => {
 
 app.get("/api/users", (req: Request, res: Response) => {
   res.send(JSON.stringify({name : "thuya", age: 30}))
+})
+
+app.post("/api/uploads", (req: Request, res: Response) => {
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    const myfile = JSON.parse(JSON.stringify(files.images));
+
+    const filepath = myfile.filepath;
+    const filename = uuidv4() + myfile.originalFilename;
+    const fileStream = fs.createReadStream(filepath);
+
+    s3.upload({
+      Bucket: "msquarefdc",
+      Key: `thuyasoe/${filename}`,
+      ACL: 'public-read',
+      Body: fileStream
+    }, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.json({ message: "file upload error form s3 upload" });
+        } else if (data) {
+          console.log(data);
+          res.json({ message: "success!" });
+        }
+      })
+  });
 })
 
 app.listen(port, () => console.log(`server is listening at ${port}`))
--- public/script.js
+++ public/script.js
@@ -1,12 +1,25 @@
+const apiUrl = localStorage.getItem("apiUrl");
+
 const fetchdata = async () => {
-  const apiUrl = localStorage.getItem("apiUrl")
   if (apiUrl) {
-    const response = await fetch(apiUrl+'/users')
+    const response = await fetch(apiUrl + "/users");
     const data = await response.json();
-    console.log(data)
+    console.log(data);
   } else {
-    window.location.href = "/api"
+    window.location.href = "/api";
   }
-}
+};
 
 fetchdata();
+
+const uploadFile = async () => {
+  const formData = new FormData();
+  const fileTag = document.getElementById("inputTag")
+  const files = [...fileTag.files]
+  files.forEach(file => formData.append("images", file))
+  const response = await fetch(apiUrl + "/uploads", {
+    method: "POST",
+    body: formData,
+  });
+  console.log(response);
+};
